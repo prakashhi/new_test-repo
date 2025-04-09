9c78798edf02d9996660f80f6fbee1446a6e4986
Dev Patel <devpatel212@gmail.com>
2025-04-09 10:59:00 +0530

    start new

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
      <h1 className='p-2 bg-red-400 font-bold'>Heloo Prakash</h1>
+     <h2 className='p-3 bg-blue-400'>Heloo Prakash</h2>
+
     </>
   )
 }
