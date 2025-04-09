78eb53350cc2cde96c209a32c5bf126b3b0a3f12
Dev Patel <devpatel212@gmail.com>
2025-04-09 10:21:17 +0530

    try

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='p-2 bg-red-400 font-bold'>Heloo Prakash</h1>
+    </>
+  )
+}
+
+export default App
